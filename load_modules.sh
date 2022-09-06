77f284ff2ee79f6078d581af61f6616bb0535743
[David Huber] <[david.huber@noaa.gov]>
2022-09-06 19:11:12 +0000

    Added a module loading script.

---
+++ load_modules.sh
@@ -0,0 +1,7 @@
+module use /scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack
+module load hpc/1.2.0 hpc-intel/18.0.5.274 hpc-impi/2018.0.4 netcdf/4.7.4
+module use /contrib/METplus/modulefiles/
+module load metplus/4.1.3
+module use /scratch1/NESDIS/nesdis-rdo2/David.Huber/miniconda3/modulefiles
+module load miniconda3/4.12.0
+conda activate aida2gfs
