a9072870de270e2b27891caf98321fc027510d68
[David Huber] <[david.huber@noaa.gov]>
2022-09-28 13:34:30 +0000

    Changed default locations of data.

--- driver.py
+++ driver.py
@@ -2,11 +2,11 @@ from aida2gfs import get_aida, aida2gfs
 import numpy as np
 from os import path
 
-aida_data = get_aida("exp004murz_pernak_predict_PREP_test_for_GFS.nc")
+aida_data = get_aida("in/exp004murz_pernak_predict_PREP_test_for_GFS.nc")
 
-gfs_path = "gfs_data"
+in_path = "in"
 ctrl_fname = "gfs_ctrl.nc"
 
 for i in range(1,7):
-   fname = path.join(gfs_path,"gfs_data.tile"+str(i)+".nc")
+   gfs_fname = path.join(in_path,"gfs_data.tile"+str(i)+".nc")
    aida2gfs(aida_data, fname, ctrl_fname, debug="yes")
