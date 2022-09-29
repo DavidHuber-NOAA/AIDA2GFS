78145df430209b411b90a9d78ea44f5b6e9b3b57
[David Huber] <[david.huber@noaa.gov]>
2022-09-29 12:45:57 +0000

    Create ranges for plot_plane.sh.

--- plot_plane.sh
+++ plot_plane.sh
@@ -13,7 +13,24 @@ fi
 #hgt=("1000" "925" "850" "700" "500" "200" "100" "10")
 hgt=("700" "500" "200")
 
-var=("PRATE" "TMP" "HGT" "UGRD" "VGRD")
+var=("WIND" "TMP" "HGT")
+
+declare -A ranges
+ranges["200HGT"]="10000 13000"
+ranges["500HGT"]="4700 6000"
+ranges["700HGT"]="2300 3300"
+ranges["200TMP"]="195 235"
+ranges["500TMP"]="220 280"
+ranges["700TMP"]="200 300"
+ranges["200WIND"]="0 120"
+ranges["500WIND"]="0 100"
+ranges["700WIND"]="0 80"
+ranges["200UGRD"]="-40 85"
+ranges["500UGRD"]="-40 80"
+ranges["700UGRD"]="-40 50"
+ranges["200VGRD"]="-50 60"
+ranges["500VGRD"]="-50 60"
+ranges["700VGRD"]="-40 40"
 
 for j in ${!var[@]}; do
    if [[ ${var[j]} == "PRATE" ]]; then
@@ -37,8 +54,14 @@ for j in ${!var[@]}; do
       else
          plot=$(echo "${plot_dir}/${prefix}_${h}mb_${x}")
       fi
-      plot_data_plane $file ${plot}.ps 'name=''"'$x'"''; level=''"'P$h'"'';' \
-         -title "$prefix ${x} at ${h}mb"
+      shortname=$h$x
+      if [[ "${ranges[$shortname]+abc}" ]]; then
+         plot_data_plane $file ${plot}.ps 'name=''"'$x'"''; level=''"'P$h'"'';' \
+            -title "$prefix ${x} at ${h}mb" -plot_range ${ranges[$shortname]}
+      else
+         plot_data_plane $file ${plot}.ps 'name=''"'$x'"''; level=''"'P$h'"'';' \
+            -title "$prefix ${x} at ${h}mb"
+      fi
       convert -rotate 90 -trim ${plot}.ps ${plot}.png
       rm ${plot}.ps
    done
