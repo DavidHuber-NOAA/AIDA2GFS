edfe403d794f814c8ae9e44d52bccd57324c34fc
[David Huber] <[david.huber@noaa.gov]>
2022-10-18 17:15:48 +0000

    Update netcdf.

--- ush/load_modules.sh
+++ ush/load_modules.sh
@@ -1,5 +1,6 @@
 module use /scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack
 module load hpc/1.2.0 hpc-intel/18.0.5.274 hpc-impi/2018.0.4
+module load netcdf/4.9.0
 module use /contrib/METplus/modulefiles/
 module load metplus/4.1.3
 module use /scratch1/NESDIS/nesdis-rdo2/David.Huber/miniconda3/modulefiles
